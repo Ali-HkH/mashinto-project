5ca3a18c4222a81900cd8f2427c2c307d1e46cef
Ali-HkH <alikheirkhah.akk@gmail.com>
2025-07-19 18:43:50 +0330

    fixed the paddings in footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,8 +4,8 @@ import FooterSocials from "./FooterSocials";
 
 function Footer() {
    return (
-      <footer className="border-t mt-8 pr-6">
-         <div className="py-3 flex flex-col items-start">
+      <footer className="border-t">
+         <div className="container flex flex-col items-start py-6">
             <FooterList />
             <FooterSocials />
             <p className="text-sm text-gray-400"><span>&copy;</span>تمامی حقوق محفوظ است</p>
